c125616e3b42d1424a8fbd392b3a5256db869807
walosha <walosha@yahoo.com>
2019-09-18 10:57:41 +0100

    created the section preview page and shop components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./components/shop/ShopPage";
+
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route path="/" exact component={HomePage}></Route>
+      <Route path="/shop" component={ShopPage}></Route>
     </div>
   );
 }
