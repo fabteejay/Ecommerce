8de387b8187fc288bbaefb9cad69632b51e300e0
walosha <walosha@yahoo.com>
2019-09-19 02:30:18 +0100

    created the sign up button and did som estyling

--- src/components/sign-in/sign-in-sign-out.component.jsx
+++ src/components/sign-in/sign-in-sign-out.component.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import SignIn from "./sign-in.componet";
+import SignUp from "../sign-up/sign-up.component";
+import "./sign-in.styles.scss";
 
 class SignInAndSignUpPage extends Component {
   render() {
     return (
       <div className="sign-in-and-sign-up">
         <SignIn></SignIn>
+        <SignUp></SignUp>
       </div>
     );
   }
