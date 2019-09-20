4561d18b300af9ed8d7f681ed6b2463130c1e60d
walosha <walosha@yahoo.com>
2019-09-20 16:21:34 +0100

    added items to dropdown menu and cart-item component was created

---
+++ src/components/cart-item/cart-item.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./cart-item.styles.scss";
+
+const CartItem = ({ name, price, imageUrl, quantity }) => {
+  console.log();
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} * {price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/components/cart/cart-dropdown.jsx
+++ src/components/cart/cart-dropdown.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButtom from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items"></div>
+      {cartItems.map(cartItem => (
+        <CartItem {...cartItem}></CartItem>
+      ))}
       <CustomButtom> Go to check out</CustomButtom>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return { cartItems };
+};
+
+export default connect(mapStateToProps)(CartDropdown);
